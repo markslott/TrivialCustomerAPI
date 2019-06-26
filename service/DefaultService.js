2c56199fb9a04487bdc80c330487d5ed8e633e19
Mark Lott <mlott@mlott-ltm5.internal.salesforce.com>
2019-06-25 22:03:29 -0500

    test

--- service/DefaultService.js
+++ service/DefaultService.js
@@ -1,5 +1,19 @@
 'use strict';
 
+var customers = [
+  {
+    "id" : "1",
+    "city" : "Montgomery",
+    "country" : "USA",
+    "postal" : "19009",
+    "state" : "PA",
+    "street" : "2725 Quarry Rd",
+    "firstName" : "Bob",
+    "lastName" : "Hodges",
+    "email" : "bhodges@example.com",
+    "phone" : "(422) 555-5262"
+  }
+]
 
 /**
  * adds a customer record
@@ -10,6 +24,7 @@
  **/
 exports.addCustomer = function(customer) {
   return new Promise(function(resolve, reject) {
+    customers.push(customer);
     resolve();
   });
 }
@@ -22,8 +37,15 @@ exports.addCustomer = function(customer) {
  **/
 exports.getCustomer = function(custId) {
   return new Promise(function(resolve, reject) {
+    var c = {};
+    customers.forEach(function(customer) {
+      console.log("customer.id = " + customer.id + " and custId = " + custId);
+      if (customer.id == custId) {
+        c = customer;
+      }
+    })
     var examples = {};
-    examples['application/json'] = "{}";
+    examples['application/json'] = c;
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
@@ -40,7 +62,7 @@ exports.getCustomer = function(custId) {
 exports.getCustomers = function() {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = "";
+    examples['application/json'] = customers;
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
